40cc6bba8758c455bce4508f598a2410b20fced6
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-05-18 00:32:07 +0000

    ew er g

--- static/sw.js
+++ static/sw.js
@@ -10,16 +10,22 @@ const dynamic = new Dynamic();
 const userKey = new URL(location).searchParams.get("userkey");
 self.dynamic = dynamic;
 
+// Handle fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
     (async () => {
       if (await dynamic.route(event)) {
         return await dynamic.fetch(event);
       }
-if (event.request.url.startsWith(`${location.origin}/a/`)) {  return await uv.fetch(event); }
-
-
+      if (event.request.url.startsWith(`${location.origin}/a/`)) {
+        return await uv.fetch(event);
+      }
       return await fetch(event.request);
     })(),
   );
+});
+
+// Capture the install event for PWA prompt
+self.addEventListener("install", event => {
+  console.log("Service Worker Installed!");
 });
