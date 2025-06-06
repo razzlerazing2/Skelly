cb7d5498d080158144be684d63587eaf6ca3c451
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-06-06 03:47:58 +0000

    esd d f

---
+++ static/assets/js/swaholebo.js
@@ -0,0 +1,3 @@
+setTimeout (function () {
+    alert("Welcome to Skelly Gaming. As you see here, on the previous page we tricked the teachers into thinking this was educational, but it is not. Enjoy the secrecy! by @razzlerazing2 :)");
+}, 2000);
